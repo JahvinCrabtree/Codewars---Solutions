8d314da1caa314d35f0ff1e544160acd211e2c15
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-06-21 20:42:52 +0100

    WordA10n

---
+++ src/6-Kyu/WordA10n.java
@@ -0,0 +1,39 @@
+public class WordA10n {
+    public String abbreviate(String string) {
+
+        String[] words = string.split("\\s+");
+        StringBuilder result = new StringBuilder();
+        
+         for (String word : words) {
+                if (word.matches("[a-zA-Z]+") && word.length() >= 4) {
+                    String abbreviation = word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+                    result.append(abbreviation);
+                } else {
+                    result.append(word);
+                }
+            }
+            
+            return result.toString();
+        }
+}
+
+/*
+The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+Example
+abbreviate("elephant-rides are really fun!")
+//          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+// words (^):   "elephant" "rides" "are" "really" "fun"
+//                123456     123     1     1234     1
+// ignore short words:               X              X
+
+// abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+// all non-word characters (*) remain in place
+//                     "-"      " "    " "     " "     "!"
+=== "e6t-r3s are r4y fun!"
+*/
+
