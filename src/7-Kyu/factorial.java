f8701fd07342da76be78b269f698867c902bbf3f
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-07-04 22:23:38 +0100

    encryptThis

--- src/7-Kyu/factorial.java
+++ src/7-Kyu/factorial.java
@@ -1,16 +1,16 @@
 public class factorial {
-    public int factorial(int n) {
-    
-        int sum = 1;
-        
-        if (n < 0 || n > 12) {
-          throw new IllegalArgumentException();
-        } 
-        
-        for (int i = 1; i <= n; i++) {
-          sum *= i;
-        }
-        
-        return sum;
-}
+  public int factorials(int n) {
+
+    int sum = 1;
+
+    if (n < 0 || n > 12) {
+      throw new IllegalArgumentException();
+    }
+
+    for (int i = 1; i <= n; i++) {
+      sum *= i;
+    }
+
+    return sum;
+  }
 }
