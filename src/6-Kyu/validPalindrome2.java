e5db74dca0985dcf0af01cf9c7de6778406e7549
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-07-08 23:07:43 +0100

    palindrome2

---
+++ src/6-Kyu/validPalindrome2.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try deleting the character at the left pointer
+                boolean skipLeft = isPalindrome(s, left + 1, right);
+                
+                // Try deleting the character at the right pointer
+                boolean skipRight = isPalindrome(s, left, right - 1);
+                
+                return skipLeft || skipRight;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+
+/*
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+ */
