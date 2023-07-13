ee9f48a5f47719e0d50cbbc41536849f64ddcb08
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-07-13 19:56:04 +0100

    maxScore + typo

---
+++ src/6-Kyu/macScore.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int maxScore(String s) {
+        int zero =0;
+        int ones = 0;
+
+        // First loop to count the zeros and ones.
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                zero++;
+            } else {
+                ones++;
+            }
+        }
+
+        int max =0;
+        int rightOnes = ones;
+        int leftZeros = 0;
+
+        // second loop iterates through again (ignoring the last character) incrementing the leftZeros or decrementing the rightOnes.
+
+        for (int i =0; i < s.length() -1; i++) {
+            if(s.charAt(i) == '0') {
+                leftZeros++;
+            } else {
+                rightOnes--;
+            }
+            max = Math.max(max, leftZeros + rightOnes);
+        }
+
+        return max;
+    }
+}
+
+/*
+Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+ 
+
+Example 1:
+
+Input: s = "011101"
+Output: 5 
+Explanation: 
+All possible ways of splitting s into two non-empty substrings are:
+left = "0" and right = "11101", score = 1 + 4 = 5 
+left = "01" and right = "1101", score = 1 + 3 = 4 
+left = "011" and right = "101", score = 1 + 2 = 3 
+left = "0111" and right = "01", score = 1 + 1 = 2 
+left = "01110" and right = "1", score = 2 + 1 = 3
+ */
